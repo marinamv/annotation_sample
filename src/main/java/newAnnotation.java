bed0fa114bd01c8b3ee839b50518a73921d182cc
Marina Kuzmina <32646401+marinamv@users.noreply.github.com>
2018-02-28 13:28:50 +0200

    Update newAnnotation.java

--- src/main/java/newAnnotation.java
+++ src/main/java/newAnnotation.java
@@ -1,4 +1,13 @@
 package PACKAGE_NAME;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface newAnnotation {
+
+    public String data() default "";
+    public String field() default "";
 }
