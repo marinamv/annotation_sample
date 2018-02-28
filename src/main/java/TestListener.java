fcdf225d16b30ad7e5757299e17369e60a065046
Marina Kuzmina <32646401+marinamv@users.noreply.github.com>
2018-02-28 13:29:27 +0200

    Update TestListener.java

--- src/main/java/TestListener.java
+++ src/main/java/TestListener.java
@@ -5,8 +5,9 @@ import org.testng.ITestResult;
 import java.lang.reflect.Method;
 
 public class TestListener implements ITestListener {
-    public void onTestStart(ITestResult result) {
-
+    public void onTestStart(ITestResult iTestResult) {
+        String DataAndFields = getNewDataAndFields(iTestResult);
+        System.out.println("Annotation data: " + DataAndFields);
     }
 
     public void onTestSuccess(ITestResult result) {
@@ -14,19 +15,19 @@ public class TestListener implements ITestListener {
         * Вот этот блок кода можно применить что бы получить аннотации над методом.
         */
         // Class myClass = SampleEmptyTest.class; заменяем на обращение к ITestResult - TestNG в нем хранит информацию о методах и результатах
-        Class myClass = result.getClass();
-        Method method = null; //
-        try {
-            method = myClass.getMethod("sampleTest"); // спрашиваем Java: "Как называется, метод внутри которого
+        //Class myClass = result.getClass();
+        //Method method = null; //
+        //try {
+        //    method = myClass.getMethod("sampleTest"); // спрашиваем Java: "Как называется, метод внутри которого
             // ты сейчас выполняешь этот кусочек кода". Проще говоря - "Где я выполнился?"
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        }
+        //} catch (NoSuchMethodException e) {
+        //    e.printStackTrace();
+        //}
 
-        TestCase testCaseAnnotation = method.getAnnotation(TestCase.class); // Где бы я не выполнялся, Java верни
+        //TestCase testCaseAnnotation = method.getAnnotation(TestCase.class); // Где бы я не выполнялся, Java верни
         // аннотацию из метода в котором я выполняюсь. Похожим образом можно сделать для класса.
-        System.out.println("ANNOTATION: " + testCaseAnnotation);
-        System.out.println("Test Case id: " + testCaseAnnotation.id()); // верни значение внутри аннотации
+       // System.out.println("ANNOTATION: " + testCaseAnnotation);
+        //System.out.println("Test Case id: " + testCaseAnnotation.id()); // верни значение внутри аннотации
     }
 
     public void onTestFailure(ITestResult result) {
@@ -48,4 +49,20 @@ public class TestListener implements ITestListener {
     public void onFinish(ITestContext context) {
 
     }
+    public String getNewDataAndFields(ITestResult iTestResult) {
+
+        String annotation = null;
+        Method method = iTestResult.getMethod().getConstructorOrMethod().getMethod();
+        try {
+            newAnnotation DataAndFields = method.getAnnotation(newAnnotation.class);
+            annotation = DataAndFields.data();
+            annotation = DataAndFields.field();
+            System.out.println("ANNOTATION: "+ DataAndFields);
+        } catch (NullPointerException e){
+    }
+    return annotation;
+    }
+
+
 }
+
